1f342909d9f379aaae745fb39dbe0175f07724b3
Soloman Garcia <soloman.garcia@gmail.com>
2020-02-20 10:54:07 -0700

    import Business.css to Business.js and export Business component

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Business.css';
 
 const business = {
     imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
@@ -37,3 +38,5 @@ class Business extends React.Component {
         );
     }
 };
+
+export default Business;
