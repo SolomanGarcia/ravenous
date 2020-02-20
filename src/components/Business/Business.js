452c3904d985e7f7414cce4b271b28634de7f349
Soloman Garcia <soloman.garcia@gmail.com>
2020-02-20 10:48:47 -0700

    create Business component

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -14,6 +14,26 @@ const business = {
 
 class Business extends React.Component {
     render() {
-        
+
+        return (
+            <div className="Business">
+                <div className="image-container">
+                    <img src={business.imageSrc} alt={business.name} />
+                </div>
+                <h2>{business.name}</h2>
+                <div className="Business-information">
+                    <div className="Business-address">
+                        <p>{business.address}</p>
+                        <p>{business.city}</p>
+                        <p>{business.state} {business.zipCode}</p>
+                    </div>
+                    <div className="Business-reviews">
+                        <h3>{business.category}</h3>
+                        <h3 className="rating">{business.rating}</h3>
+                        <p>{business.reviewCount}</p>
+                    </div>
+                </div>
+            </div>
+        );
     }
-}
+};
