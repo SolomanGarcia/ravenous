70c6ebe6d479edefdffd853d784fe181dfaff76c
Soloman Garcia <soloman.garcia@gmail.com>
2020-02-20 12:27:28 -0700

    build sortByOptions object in SearchBar.js

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SearchBar.css';
+
+const sortByOptions = {
+    'Best Match': 'best_match',
+    'Highest Rated': 'rating',
+    'Most Reviewed': 'review_count'
+};
+
+class SearchBar extends React.Component {
+
+};
