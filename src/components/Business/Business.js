e4194ad27675628fb71675907c70c6c1ea3c8ac5
Soloman Garcia <soloman.garcia@gmail.com>
2020-02-20 11:53:22 -0700

    fix minor issues with Business.js Business component

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -11,7 +11,7 @@ const business = {
     category: 'Italian',
     rating: 4.5,
     reviewCount: 90
-}
+};
 
 class Business extends React.Component {
     render() {
@@ -26,12 +26,12 @@ class Business extends React.Component {
                     <div className="Business-address">
                         <p>{business.address}</p>
                         <p>{business.city}</p>
-                        <p>{business.state} {business.zipCode}</p>
+                        <p>{`${business.state} ${business.zipCode}`}</p>
                     </div>
                     <div className="Business-reviews">
-                        <h3>{business.category}</h3>
-                        <h3 className="rating">{business.rating}</h3>
-                        <p>{business.reviewCount}</p>
+                        <h3>{business.category.toUpperCase()}</h3>
+                        <h3 className="rating">{`${business.rating} stars`}</h3>
+                        <p>{`${business.reviewCount} reviews`}</p>
                     </div>
                 </div>
             </div>
