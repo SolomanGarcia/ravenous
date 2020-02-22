d85e7b610f8a29273ca66b9dfcff62f1ed49fe2f
Soloman Garcia <soloman.garcia@gmail.com>
2020-02-22 09:24:08 -0700

    create handleSearch method in SearchBar.js

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -43,6 +43,11 @@ class SearchBar extends React.Component {
         this.setState({ location: event.target.value });
     }
 
+    handleSearch(event) {
+        this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
+        event.preventDefault();
+    }
+
     renderSortByOptions() {
 
         return Object.keys(this.sortByOptions).map(sortByOption => {
