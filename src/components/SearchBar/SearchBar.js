8c2f8f6b44ed66341a703a16e77b2eee7169d996
Soloman Garcia <soloman.garcia@gmail.com>
2020-02-22 09:25:15 -0700

    bind handleSearch

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -15,6 +15,7 @@ class SearchBar extends React.Component {
 
         this.handleTermChange = this.handleTermChange.bind(this);
         this.handleLocationChange = this.handleLocationChange.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
 
         this.sortByOptions = {
             'Best Match': 'best_match',
