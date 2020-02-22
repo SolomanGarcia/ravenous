2a3a73594c2106fe101d24e8377f4287386d8ad3
Soloman Garcia <soloman.garcia@gmail.com>
2020-02-22 11:15:16 -0700

    add searchYelp constructor

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -9,7 +9,7 @@ class BusinessList extends React.Component {
             <div className="BusinessList">
                 { 
                 this.props.businesses.map((business) => {
-                    return <Business business={business} />;
+                    return <Business business={business} key={business.id}/>;
                 })
                 }
             </div>
