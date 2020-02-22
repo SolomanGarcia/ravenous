f7890c7d3347191243be017f63a6f1e544f3827b
Soloman Garcia <soloman.garcia@gmail.com>
2020-02-22 10:55:10 -0700

    build and export Yelp module

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,7 +1,32 @@
 const apiKey = 'Spp04X6tlK0AIcPhOYG9eueKWo9jaoBDfzylbX2wmLF0yn4CBzgNmY_osREwNP_6qgJ5pbg3NPK_92h2AVsQ_sY4MBiDtWkj47TMzgKrTvP4I0Dg1kTo_XBAkWJRXnYx';
 
-const yelp = {
-    search(term, location, sortBy) {
-        return fetch(`https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`);
+const Yelp = {
+    searchYelp(term, location, sortBy) {
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+            headers: {
+                Authorization: `Bearer ${apiKey}`
+            }
+        }).then(response => {
+            return response.json();
+        }).then(jsonResponse => {
+            if (jsonResponse.businesses) {
+                return jsonResponse.businesses.map(business => {
+                    return {
+                        id: business.id,
+                        imageSrc: business.image_url,
+                        name: business.name,
+                        address: business.location.address1,
+                        city: business.location.city,
+                        state: business.location.state,
+                        zipCode: business.location.zip_code,
+                        category: business.categories[0].title,
+                        rating: business.rating,
+                        reviewCount: business.review_count
+                    }
+                });
+            }
+        });
     }
 };
+
+export default Yelp;
