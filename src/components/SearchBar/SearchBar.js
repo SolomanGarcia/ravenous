de5c93895e2251309a36184626500ed31b379a8b
Soloman Garcia <soloman.garcia@gmail.com>
2020-02-22 08:15:22 -0700

    add SearchBar constructor to SearchBar.js

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -8,6 +8,15 @@ const sortByOptions = {
 };
 
 class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: '',
+            location: '',
+            sortBy: 'best_match'
+        };
+    }
+
     renderSortByOptions() {
 
         return Object.keys(sortByOptions).map(sortByOptions => {
